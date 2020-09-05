5e76d711e8fbbe52e897bf24ad76c7dd51248e21
andres giraldo <drawl9988@gmail.com>
2020-09-04 22:24:05 -0500

    breadcrumbs dinamicos

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -15,10 +15,10 @@ const routes: Routes = [
         path: 'dashboard',
         component: PagesComponent,
         children: [
-          { path: '', component: DashboardComponent },
-          { path: 'grafica1', component: Grafica1Component },
-          { path: 'progress', component: ProgressComponent },
-          { path: 'account-settings', component: AccountSettingsComponent },
+          { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+          { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Grafica' } },
+          { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
+          { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Tema' } },
     
         ]
       },
